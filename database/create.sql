ef4ded144de9a70cef82cc59e718b67b077439da
jackson1010 <zinwin_1994@gmail.com>
2023-04-28 15:49:32 +0800

    second commit

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,12 @@
 -- Task 1
 -- Write SQL statements in this file to create the brewery database and 
 -- populate the database with the given SQL files
+
+create database brewery 
+
+source C:\Users\speed\Desktop\assessment_template\database\beers.sql
+source C:\Users\speed\Desktop\assessment_template\database\breweries.sql
+source C:\Users\speed\Desktop\assessment_template\database\categories.sql
+source C:\Users\speed\Desktop\assessment_template\database\geocodes.sql
+source C:\Users\speed\Desktop\assessment_template\database\styles.sql
+
